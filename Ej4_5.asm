33a53b9720311a794c6f1110949e0ca7a987155a
Bruno A. Genero <generobruno@gmail.com>
2022-04-27 14:08:24 -0300

    Cambios menores

--- Ej4_5.asm
+++ Ej4_5.asm
@@ -1,7 +1,7 @@
 ; Ejercicio 4.5
 ; Se desea que al apretar el pulsador conectado a RA4 parpadeen, a una frecuencia
 ; de 0.5Hz, los 8 LEDs conectados en cátodo común a los 8 terminales del puerto D
-; de un microcontrolador PIC 16F887. Dicho parpadeo se debe interrumpir
+; del PIC 16F887. Dicho parpadeo se debe interrumpir
 ; durante unos instantes (3 segundos) si se aprieta el pulsador conectado al
 ; terminal RB0. Inicialmente, los LEDs están apagados. El oscilador es de 4MHz. 
     
